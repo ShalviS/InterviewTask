470b0e08a31b6963fec73ff6cf6ea2c262031cb1
BlissApp <shalvi@fourvector.in>
2019-09-03 10:27:07 +0530

    Define Retrofit client for Application

--- app/src/main/java/com/e/ar_interview/remote/RetrofitClient.java
+++ app/src/main/java/com/e/ar_interview/remote/RetrofitClient.java
@@ -13,7 +13,7 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String url) {
-        System.out.println("url==========" + url);
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(url)
