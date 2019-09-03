ec41e3c8286a7fb63f03413073919fc12927fd12
BlissApp <shalvi@fourvector.in>
2019-09-03 10:26:05 +0530

    Api calling utility data

--- app/src/main/java/com/e/ar_interview/remote/ApiUtils.java
+++ app/src/main/java/com/e/ar_interview/remote/ApiUtils.java
@@ -1,12 +1,13 @@
 package com.e.ar_interview.remote;
+
 /**
  * Created by shalvi on 9/2/2019.
  */
 public class ApiUtils {
 
-    public static final String BASE_URL ="https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
+    public static final String BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
 
-    public static ApiCall getCityDetails(){
+    public static ApiCall getCityDetails() {
         return RetrofitClient.getClient(BASE_URL).create(ApiCall.class);
     }
 
